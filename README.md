4f8096bc9845d404b112dcf0f8c56f6843fe527f
Evair Garcia <evair.garcia@geofusion.com.br>
2020-08-17 19:25:22 -0300

    Inicial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# forca
+
+Projeto feito como resultado do curso 'Clojure: uma introdução via jogo' da Alura.
+O projeto apresenta a implementação de um jogo de forca utilizando o paradigma funcional por meio da linguagem de programação Clojure
+
+## Usage
+Para executar o jar do projeto execute o comando:
+
+    $ java -jar forca-0.1.0-standalone.jar
+
+
+## License
+
+This program and the accompanying materials are made available under the
+terms of the Eclipse Public License 2.0 which is available at
+http://www.eclipse.org/legal/epl-2.0.
+
+This Source Code may also be made available under the following Secondary
+Licenses when the conditions for such availability set forth in the Eclipse
+Public License, v. 2.0 are satisfied: GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or (at your
+option) any later version, with the GNU Classpath Exception which is available
+at https://www.gnu.org/software/classpath/license.html.
---
+++ src/forca/core.clj
@@ -0,0 +1,58 @@
+(ns forca.core
+  (:gen-class))
+
+(def total-de-vidas 6)
+
+(def palavra-secreta "MELANCIA")
+
+(defn perdeu [] (print "Você perdeu"))
+
+(defn letras-faltantes [palavra acertos]
+  (remove (fn [letra] (contains? acertos (str letra))) palavra))
+
+(defn acertou-a-palavra-toda? [palavra acertos] 
+  (empty? (letras-faltantes palavra acertos)))
+
+(defn ganhou [] (print "Ganhou"))
+
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra]
+  (.contains palavra chute))
+
+(defn imprime-forca [vidas palavra acertos]
+  (println "Vidas" vidas)
+  (doseq [letra (seq palavra)] (if (contains? acertos (str letra))
+      (print letra " ") (print "_" " ")
+  ))
+  (println))
+
+(defn jogo [vidas palavra acertos] 
+  (imprime-forca vidas palavra acertos)
+  (cond 
+    (= vidas 0) (perdeu)
+    (acertou-a-palavra-toda? palavra acertos) (ganhou)
+    :else
+    (let [chute (le-letra!)]
+      (if (acertou? chute palavra)
+      (do 
+       (println "Acertou Chute!")
+       (recur vidas palavra (conj acertos chute))
+      )
+      (do
+        (println "Errou o chute!")
+        (recur (dec vidas) palavra acertos)
+      )
+      ))))
+
+(defn avalia-chute [chute vidas palavra acertos]
+  (if (acertou? chute palavra)
+    (jogo vidas palavra (conj acertos chute))
+    (jogo (dec vidas) palavra acertos)))
+
+(defn comeca-o-jogo []
+  (jogo total-de-vidas palavra-secreta #{}))
+
+(defn -main
+  [& args]
+  (comeca-o-jogo))
